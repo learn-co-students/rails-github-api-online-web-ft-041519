e1dea2420c16b95b8a088c971e7c0311777d18f7
Alison Golden <alisong779@yahoo.com>
2019-08-14 13:05:38 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :authenticate_user
 
   private
 
   def authenticate_user
-  end
+      @user ||= User.find_by_id(session[:user_id])
+    end
 
   def logged_in?
+    !!session[:token]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
+  BASE_URL = "https://github.com/login/oauth/authorize"
+
   def create
+    resp = Faraday.get(BASE_URL) do |req|
+      req.params["client_id"] = ENV["GITHUB_CLIENT"]
+    end
+
+    body = JSON.parse(resp.body)
+    binding.pry
   end
 end
